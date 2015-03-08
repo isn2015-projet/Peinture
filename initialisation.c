521042151a9eeadb1dbe9bf5a13ca60e057817ea
isn2015-projet <contact@ntset.fr>
2015-03-08 23:48:10 +0100

    Update initialisation.c

--- initialisation.c
+++ initialisation.c
@@ -2,27 +2,29 @@
 #define NB_BOUTON 13
 #define COULEUR_FOND couleur_RGB(255,255,255)
 #define COULEUR_COLONNE couleur_RGB(230,230,230)
+#define COULEUR_TEXTE couleur_RGB(0,0,0)
 #define COULEUR_BORDURE couleur_RGB(150,150,150)
 /*
-Dimension d'un bouton: 24px - 24px
-1: Pinceau
-2: Brosse
-3: Aérographe
-4: Remplissage
-5: Gomme
-6: Pipette
-7: Ligne
-8: Rectangle
-9: Polygone
-10: Ellipse
-11: Rectangle Arrondi
-12: Selection
-13: Texte
-*/
+ Dimension d'un bouton: 24px - 24px
+ 1: Pinceau
+ 2: Brosse
+ 3: Aérographe
+ 4: Remplissage
+ 5: Gomme
+ 6: Pipette
+ 7: Ligne
+ 8: Rectangle
+ 9: Polygone
+ 10: Ellipse
+ 11: Rectangle Arrondi
+ 12: Selection
+ 13: Texte
+ */
 //----------------------------------------
 
 //on déclare les variables dont nous aurons besoin
 int bouton[NB_BOUTON] = {0};
+char mode[40] = "Outil choisi: Aucun";
 
 
 //on développe notre fonction d'initialisation du projet
@@ -32,7 +34,7 @@ void initialisation_projet(char *titre, int width, int height, int *bouton) {
     
     initialiser_fenetre(width, height, titre);
     affiche_auto_off();
-
+    
     
     
     //Création du tableau bouton où nous stockerons les informations liés aux boutons
@@ -68,8 +70,14 @@ void affichage_interface(int width, int height) {
     p1.x = 50; p1.y = height;
     p2.x = 50; p2.y = 50;
     dessine_ligne(p1, p2, COULEUR_BORDURE);
-
-
+    
+    p1.x = width-170; p1.y = 43;
+    p2 = position_souris();
+    char coordonnees[30];
+    sprintf(coordonnees, "Position: %dx%d", p2.x-50, p2.y-50);
+    dessine_texte(coordonnees,12,p1,COULEUR_TEXTE);
+    p1.y -= 20;
+    dessine_texte(mode,12,p1,COULEUR_TEXTE);
+    
     affiche_tout();
 }
-
