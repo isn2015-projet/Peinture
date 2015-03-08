9a89af80a3c2f2698e56dd6a42c6980b7d2be62c
isn2015-projet <contact@ntset.fr>
2015-03-08 17:54:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,11 @@
 
 int main( int argc, char** argv )
 {
-
-
-
+    initialisation_projet(/*titre*/"Peinture", /*width*/1200, /*height*/700, bouton);
+    
+    
+    
+    
+    attendre(5000);
     quitter(0);
 }
