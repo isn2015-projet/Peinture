6809a71c6051516f4e7782efd451db974d5b1514
isn2015-projet <contact@ntset.fr>
2015-03-09 15:49:44 +0100

    Update declarations.c

--- declarations.c
+++ declarations.c
@@ -1,1 +1,36 @@
+//defines
+#define NB_BOUTON 13
+#define COULEUR_FOND couleur_RGB(255,255,255)
+#define COULEUR_COLONNE couleur_RGB(230,230,230)
+#define COULEUR_TEXTE couleur_RGB(0,0,0)
+#define COULEUR_BORDURE couleur_RGB(150,150,150)
+#define WIDTH 1200
+#define HEIGHT 700
+#define LARGEUR_MENU 50
 
+//variables utilisées:
+int bouton[NB_BOUTON] = {0};
+char mode[200] = "Outil choisi: Aucun";
+
+
+
+
+
+
+/*
+ Dimension d'un bouton: 24px - 24px
+ 1: Pinceau
+ 2: Brosse
+ 3: Aérographe
+ 4: Remplissage
+ 5: Gomme
+ 6: Pipette
+ 7: Ligne
+ 8: Rectangle
+ 9: Polygone
+ 10: Ellipse
+ 11: Rectangle Arrondi
+ 12: Selection
+ 13: Texte
+ */
+ 
