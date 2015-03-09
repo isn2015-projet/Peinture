8c946eec061bf85676e024bfdd9d69219322cdae
isn2015-projet <contact@ntset.fr>
2015-03-09 15:49:25 +0100

    Update initialisation.c

--- initialisation.c
+++ initialisation.c
@@ -1,34 +1,5 @@
-//--------------PARAMETRES---------------
-#define NB_BOUTON 13
-#define COULEUR_FOND couleur_RGB(255,255,255)
-#define COULEUR_COLONNE couleur_RGB(230,230,230)
-#define COULEUR_TEXTE couleur_RGB(0,0,0)
-#define COULEUR_BORDURE couleur_RGB(150,150,150)
-/*
- Dimension d'un bouton: 24px - 24px
- 1: Pinceau
- 2: Brosse
- 3: Aérographe
- 4: Remplissage
- 5: Gomme
- 6: Pipette
- 7: Ligne
- 8: Rectangle
- 9: Polygone
- 10: Ellipse
- 11: Rectangle Arrondi
- 12: Selection
- 13: Texte
- */
-//----------------------------------------
-
-//on déclare les variables dont nous aurons besoin
-int bouton[NB_BOUTON] = {0};
-char mode[200] = "Outil choisi: Aucun";
-
-
 //on développe notre fonction d'initialisation du projet
-void initialisation_projet(char *titre, int width, int height, int *bouton) {
+void initialisation_projet(char *titre, int width, int height) {
     printf("Lancement de l'initialisation du projet...\n");
     
     
@@ -56,27 +27,27 @@ void affichage_interface(int width, int height) {
     //colonne de gauche
     POINT p1, p2;
     p1.x = 0; p1.y = 0;
-    p2.x = 50; p2.y = height;
+    p2.x = LARGEUR_MENU; p2.y = height;
     dessine_rectangle_plein(p1, p2, COULEUR_COLONNE);
     
     //ligne du bas
     p1.x = 0; p1.y = 0;
-    p2.x = width; p2.y = 50;
+    p2.x = width; p2.y = LARGEUR_MENU;
     dessine_rectangle_plein(p1, p2, COULEUR_COLONNE);
     
     //bordures
-    p1.x = 0; p1.y = 50;
-    p2.x = width; p2.y = 50;
+    p1.x = 0; p1.y = LARGEUR_MENU;
+    p2.x = width; p2.y = LARGEUR_MENU;
     dessine_ligne(p1, p2, COULEUR_BORDURE);
-    p1.x = 50; p1.y = height;
-    p2.x = 50; p2.y = 50;
+    p1.x = LARGEUR_MENU; p1.y = height;
+    p2.x = LARGEUR_MENU; p2.y = LARGEUR_MENU;
     dessine_ligne(p1, p2, COULEUR_BORDURE);
     //
     //informations bottom right (outil sélectionné + position curseur)
     p1.x = width-170; p1.y = 43;
     p2 = position_souris();
     char coordonnees[30];
-    sprintf(coordonnees, "Position: %dx%d", p2.x-50, p2.y-50);
+    sprintf(coordonnees, "Position: %dx%d", p2.x-LARGEUR_MENU, p2.y-LARGEUR_MENU);
     dessine_texte(coordonnees,12,p1,COULEUR_TEXTE);
     p1.y -= 20;
     dessine_texte(mode,12,p1,COULEUR_TEXTE);
