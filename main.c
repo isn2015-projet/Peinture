8fb9215122edeae4e7524b67f47d9ccc2028e7f0
isn2015-projet <contact@ntset.fr>
2015-03-09 15:49:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,24 +3,14 @@
 #include "fonctions.h"
 #include "initialisation.c"
 
-/*idée: rassembler toutes les déclarations de fonction et variable dans un même fichier
---> declarations.h par exemple à inclure au tout début, ça permet de déclarer une fonction sans
-pour autant la développer, puisque qu'on peut le faire ensuite
-
-idée: il faudrait qu'on développe le fichier initialisation ensemble, 
-mais pas en même temps, comme ça on part sur la même base*,
-puis, pour les fonctions, qui sont les éléments importants, au lieu d'inclure fonctions.c on inclut
-fonctions.h où, dans ce fichier on inclut les fichiers de fonctions.c, comme ça on peut travailler en même temps
-sans modifier la version de l'autre sur une fonction différente chacun */
-
 
 
 int main( int argc, char** argv )
 {
-    initialisation_projet(/*titre*/"Peinture", /*width*/1200, /*height*/700, bouton);
+    initialisation_projet("Peinture", WIDTH, HEIGHT);
     while(1)
     {
-        affichage_interface(1200, 700);
+        affichage_interface(WIDTH, HEIGHT);
         attendre(20);
     }
     
