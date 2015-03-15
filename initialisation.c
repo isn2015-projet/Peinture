56020a77d701cd0993f22a5b21ef86c69331738a
Nicolas TROGNOT <elnicolo78@gmail.com>
2015-03-15 12:01:59 +0100

    Update initialisation.c

--- initialisation.c
+++ initialisation.c
@@ -149,7 +149,9 @@ void affichage_interface(int width, int height) {
     //Affichage des options
     p1.x = 3; p2.x = LARGEUR_MENU-3;
     p1.y -= 40; p2.y -= 80;
-    dessine_rectangle_plein(p1,p2,COULEUR_BORDURE);
+    dessine_rectangle(p1,p2,COULEUR_BORDURE);
+    p1.x += 10;
+    dessine_texte("opt.",12,p1,COULEUR_TEXTE);
     ///////////////////
 
     
