a441bb62583580520f6403a93f84910509480cdd
Nicolas TROGNOT <elnicolo78@gmail.com>
2015-03-16 23:07:55 +0100

    Update gestion.c

--- fonctions/gestion.c
+++ fonctions/gestion.c
@@ -3,7 +3,7 @@ BOOL Bouton(POINT p, int x_min, int x_max, int y_min, int y_max){
     else {return False;}
 }
 
-void gestion_palette(){
+void gestion_interface(){
     POINT clic_gch,clic_drt;
     int i;
 
@@ -19,5 +19,49 @@ void gestion_palette(){
             if(Bouton(clic_gch,-75+i*15,-60+i*15,10,25)==true){colour.pri=palette[i];}
             if(Bouton(clic_drt,-75+i*15,-60+i*15,10,25)==true){colour.sec=palette[i];}
         }
+    }
+    
+    
+    
+    int i_bouton = 0, duet = 0, bordure_bouton_top = 10;
+    POINT p1, p2;
+    int i_selection_outil;
+    for(i_bouton = 0; i_bouton < NB_BOUTON; i_bouton++)
+    {
+        if(i_bouton == 0)
+        {
+            p1.y = HEIGHT-bordure_bouton_top;
+            p2.y = HEIGHT-bordure_bouton_top-24;
+        }
+        if(duet == 1) {
+            p1.x = 1;
+            p2.x = 25;
+            p1.x = 25;
+            p2.x = 49;
+        }
+        else {
+            p1.x = 1;
+            p2.x = 25;
+            
+            p1.y -= 24;
+            p2.y -= 24;
+        }
+        if(Bouton(clic_gch,p1.x,p2.x,p2.y,p1.y)==true)
+        {
+            printf("%d\n",i_bouton);
+            p1.x = WIDTH-170; p1.y = 23;
+            sprintf(mode, "Outil n%d", i_bouton+1);
+            dessine_texte(mode,12,p1,COULEUR_TEXTE);
+            for (i_selection_outil = 0; i_selection_outil < NB_BOUTON; i_selection_outil++) {
+                if(i_selection_outil==i_bouton)
+                {
+                    selection_outil[i_selection_outil] = true;
+                }
+                else {
+                    selection_outil[i_selection_outil] = false;
+                }
+            }
+        }
+        duet = 1 - duet;
     }
 }
