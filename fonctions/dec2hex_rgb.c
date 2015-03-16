aa5e88f24bf4461dfe7ab17392ab3bc54f552a0b
isn2015-projet <contact@ntset.fr>
2015-03-16 15:42:07 +0100

    Create dec2hex_rgb.c

---
+++ fonctions/dec2hex_rgb.c
@@ -0,0 +1,11 @@
+int dec2hex_rgb(int parametre, int intColor) {
+    if(parametre==0) { // on extrait le rouge
+        return (intColor & 0xff0000) >> 16;
+    }
+    else if(parametre==1) { // on extrait le vert
+        return (intColor & 0x00ff00) >> 8;
+    }
+    else { // on extrait le bleu
+        return (intColor & 0x0000ff);
+    }
+}
