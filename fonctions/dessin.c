af3c9de738e66cf0cd461e1c3e0aa7c800c4c276
Dfenyi <d.fenyi@gmail.com>
2015-03-18 16:24:55 +0100

    Update dessin.c

--- fonctions/dessin.c
+++ fonctions/dessin.c
@@ -14,3 +14,29 @@ void pinceau(){
         attendre(10);
         }
 }
+
+void aerographe(){
+    POINT p,P;
+    float cosinus=0;
+    
+    p=position_souris();
+    
+    if/*while*/(bouton_appuye(CLIC_GCH)==true && p.x>50 && p.y>50){
+        cosinus=(reel_aleatoire()+entier_aleatoire(4))*2;
+            
+        P.x=floor(p.x+cos(cosinus)*entier_aleatoire(10));
+        P.y=floor(p.y+sin(cosinus)*entier_aleatoire(10));
+            
+        dessine_pixel(P,colour.pri);
+        affiche_tout();
+    }
+    
+    if/*while*/(bouton_appuye(CLIC_DRT)==true && p.x>50 && p.y>50){
+        cosinus=(reel_aleatoire()+entier_aleatoire(4))*2;
+            
+        P.x=floor(p.x+cos(cosinus)*entier_aleatoire(10));
+        P.y=floor(p.y+sin(cosinus)*entier_aleatoire(10));
+        
+        dessine_pixel(P,colour.sec);
+    }
+}
