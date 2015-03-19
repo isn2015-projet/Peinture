3a5856fd4bc1cc1339fef7624a8f0da50ee03aa8
Dfenyi <d.fenyi@gmail.com>
2015-03-19 20:51:31 +0100

    Update dessin.c

--- fonctions/dessin.c
+++ fonctions/dessin.c
@@ -5,13 +5,11 @@ void pinceau(){
     if/*while*/(bouton_appuye(CLIC_GCH)==true && p.x>50 && p.y>50){
         p=position_souris();
         dessine_cercle_plein(p,4,colour.pri);
-        attendre(10);
         }
 
     if/*while*/(bouton_appuye(CLIC_DRT)==true && p.x>50 && p.y>50){
         p=position_souris();
         dessine_cercle_plein(p,4,colour.sec);
-        attendre(10);
         }
 }
 
