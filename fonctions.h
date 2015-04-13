5b2f33c90a2ded9cebbe21ab5f99a926856bcedd
Dfenyi <d.fenyi@gmail.com>
2015-04-13 15:52:55 +0200

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -2,5 +2,6 @@
 #include "fonctions/dec2hex_rgb.c"
 #include <math.h>
 #include "fonctions/dessin.c"
+#include "fonctions/utilitaire.c"
 #include "fonctions/gestion.c"
 #include "fonctions/options.c"
