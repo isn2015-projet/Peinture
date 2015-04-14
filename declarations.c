751eeee8a30f611c816d4170ddf54c27f68858ad
Nicolas TROGNOT <elnicolo78@gmail.com>
2015-04-14 17:54:56 +0200

    Update declarations.c

--- declarations.c
+++ declarations.c
@@ -9,26 +9,36 @@
 #define HEIGHT 700
 #define LARGEUR_MENU 50
 
-//variables utilisées:
+//Pour stocker les boutons et ses icons:
 int bouton[NB_BOUTON];
 int bouton_icons[NB_BOUTON][576];
-char mode[200] = "Outil : Pinceau";
-COULEUR palette[NB_COULEUR];
-int option = 1; // par défaut: à 1 (plus petit)
 
+
+
+char mode[200] = "Outil : Pinceau";
+
+//variable qui stockera les couleurs de la palette
+COULEUR palette[NB_COULEUR];
+
+
+//structure qui représentera la couleur principale et secondaire du document:
 typedef struct{
 	COULEUR pri;
 	COULEUR sec;
 }couleurs;
 
+//création de la variable couleur (pour le moment elle vaut null mais on assignera des valeurs dans l'initialisation):
 couleurs colour;
 
+// Variable qui stockera quel outil nous avons sélectionné, par défaut: Pinceau
 int outil=0;
+int option = 1; // par défaut: à 1 (plus petit, var max: 3)
 
+//Structure qui représente notre document vierge sur lequel on va dessiner:
 IMAGE sans_titre;
 
 /*
- Dimension d'un bouton: 24px - 24px
+ Dimension d'un bouton: 24px - 24px (prendre n-1)
  1: Pinceau
  2: Brosse
  3: Aérographe
