7d4baa0ad60051fe8190b6b5d1d82db0d2c6d463
Nicolas TROGNOT <elnicolo78@gmail.com>
2015-05-04 22:18:03 +0200

    Update utilitaire.c

--- fonctions/utilitaire.c
+++ fonctions/utilitaire.c
@@ -82,12 +82,10 @@ void sauvegarde_document() {
     (void) fclose(fp);
     printf("Sauvegarde ok !\n");
     
-    
-    time_t rawtime;
-    struct tm * timeinfo;
-    
-    time ( &rawtime );
-    timeinfo = localtime ( &rawtime );
-    
-    sprintf(derniere_sauvegarde, "%s", asctime (timeinfo) );
+
+    time_t now = time (NULL);
+    struct tm tm_now = *localtime (&now);
+    char s_now[sizeof "JJ/MM/AAAA HH:MM:SS"];
+    strftime (s_now, sizeof s_now, "%d/%m/%Y %H:%M:%S", &tm_now);
+    sprintf(derniere_sauvegarde, "%s", s_now);
 }
