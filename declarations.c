8d8f5c8974d1230bf4e804ae3010d32da59c307f
Dfenyi <d.fenyi@gmail.com>
2015-05-11 16:26:15 +0200

    Update declarations.c

--- declarations.c
+++ declarations.c
@@ -1,5 +1,5 @@
 //defines
-#define NB_BOUTON 13
+#define NB_BOUTON 11
 #define NB_COULEUR 18
 #define COULEUR_FOND couleur_RGB(255,255,255)
 #define COULEUR_COLONNE couleur_RGB(230,230,230)
@@ -59,6 +59,4 @@ char derniere_sauvegarde[200] = "jamais";
  9: Polygone
  10: Ellipse
  11: Rectangle Arrondi
- 12: Selection
- 13: Texte
  */
