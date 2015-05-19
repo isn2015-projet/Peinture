496f0b631cb66a5328fe15f5568631b50872dde0
Dfenyi <d.fenyi@gmail.com>
2015-05-19 22:34:31 +0200

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -1,7 +1,7 @@
 #include "fonctions/ouvre_fichier_icon.c"
 #include "fonctions/dec2hex_rgb.c"
 #include <math.h>
+#include "fonctions/options.c"
 #include "fonctions/utilitaire.c"
 #include "fonctions/dessin.c"
 #include "fonctions/gestion.c"
-#include "fonctions/options.c"
